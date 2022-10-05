f4dadce70f15aac8b1f2279ece792a6984079249
WENZHENGZH4NG <113771649+WENZHENGZH4NG@users.noreply.github.com>
2022-10-05 16:46:41 +0100

    add some commons to proj1.r

--- proj1.r
+++ proj1.r
@@ -1,52 +1,60 @@
 ## Ziwen Zhong S2326022
-## Wenzheng Zhang
-## Tianqi Dai
+## Wenzheng Zhang S2310185
+## Tianqi Dai S2302524
 
 ## Contribution
 
 
 ####################################################################
+
+## steep 3
+## Read the file into R
+# setwd("")
 a <- scan("pg10.txt", what="character", skip=104) ## skip contents
 n <- length(a)
 a <- a[-((n-2886):n)] ## strip license
 a <- a[-grep("[0123456789]:[0123456789]", a)] ## strip out verse numbers
+a
 
+## step4
 ## pre-processing: definition of the function split_punct
 split_punct <- function(a){
-  ii <- grep('[,.;!:?]', a)
-  xs <- rep("", length(a)+length(ii))
-  iis <- ii + 1:length(ii)
-  xs[iis] <- substr(a[ii], nchar(a[ii]), nchar(a[ii]))
-  xs[-iis] <- gsub('[,.;!:?]', '', a)
-  return(xs)
+  ii <- grep('[,.;!:?]', a) ## locate the word with punctuation
+  xs <- rep("", length(a)+length(ii)) ## create a space to store the final words and punctuation
+  iis <- ii + 1:length(ii) ## calculate the location of the punctuation
+  xs[iis] <- substr(a[ii], nchar(a[ii]), nchar(a[ii]))  ## fill the space with punctuation
+  xs[-iis] <- gsub('[,.;!:?]', '', a) ## fill the rest with word
+  return(xs) ## return the final vector which is word-punctuation apart
 }
 
-## use split_punct to process a
-a <- split_punct(a)
-a <- tolower(a)
+## step 5
+## use split_punct function to process a
+a <- split_punct(a) 
 
+## step 6
 ## create b
-b <- unique(a)
-e <- match(a, b)
-fre <- tabulate(e)
-b <- b[which(fre>=500)]
+a <- tolower(a) ## lowercase the vector a
+b <- unique(a) ## find the vector b of unique words in the bible text
+e <- match(a, b) ## find the index of which element in b each element of a corresponds to
+fre <- tabulate(e) ## calculate the frequency of every unique word in a
+thre_num <- sort(fre, decreasing=T)[500] ## arrange the frequency vector to a decreasing order to find the threshold number
+b <- b[which(fre>=thre_num)] ## output about 500 most common words, store in vector b
 
-## generate T
-e <- match(a, b)
-t <- cbind(c(NA,NA,e),c(NA,e,NA),c(e,NA,NA))
-# t <- t[complete.cases(t),]
+## generate T array
+e <- match(a, b) ## find the index of which element in b each element of a corresponds to
+t <- cbind(c(NA,NA,e),c(NA,e,NA),c(e,NA,NA)) ## combine 3 vectors, each vector followed by the same vector shifted by one place
 
-MT <- array(0,c(length(b),length(b),length(b)))
-count.na <- rowSums(is.na(t))
+MT <- array(0,c(length(b),length(b),length(b))) ## initialize an empty MT array to count
+t <- t[rowSums(is.na(t))==0,] ## drop the word triplets that contain NA
 for (index in 1:nrow(t)){
-  if (count.na[index] == 0){
+  ## loop through each row of word triples, count its frequency by add 1 to MT[i, k, j] every time [i, k, j] shows up.
     i <- t[index,1]
-    j <- t[index,2]
-    k <- t[index,3]
-    MT[i,j,k] <- MT[i,j,k] + 1
-  }
+    k <- t[index,2]
+    j <- t[index,3]
+    MT[i,k,j] <- MT[i,k,j] + 1
 }
 
+## loop through each vector [i,k,], divide the sum of the vector to obtain the probability
 for (index1 in 1:length(b)){
   for (index2 in 1:length(b)) {
     if (sum(MT[index1,index2,])!=0){
@@ -55,42 +63,49 @@ for (index1 in 1:length(b)){
   }
 }
 
-## Generate A
-e <- match(a, b)
-t <- cbind(c(NA,e),c(e,NA))
-# t <- t[complete.cases(t),]
+## Generate A array
+e <- match(a, b) ## translate vector of words to vector of index corresponds to b.
+t <- cbind(c(NA,e),c(e,NA))  ## combine 2 vectors, the first vector followed by the same vector shifted by one place
 
-MA <- array(0,c(length(b),length(b)))
-count.na <- rowSums(is.na(t))
+MA <- array(0,c(length(b),length(b)))  ## initialize an empty MA array to count
+t <- t[rowSums(is.na(t))==0,]  ## drop the word doubles that contain NA
+## loop through each row of word doubles, count its frequency by add 1 to MA[i, k] every time [i, k] shows up.
 for (index in 1:nrow(t)){
-  if (count.na[index] == 0){
     i <- t[index,1]
-    j <- t[index,2]
-    MA[i,j] <- MA[i,j] + 1
-  }
+    k <- t[index,2]
+    MA[i,k] <- MA[i,k] + 1
 }
 
+## loop through each vector [i,], divide the sum of the vector to obtain the probability
 for (index1 in 1:length(b)){
   if (sum(MA[index1,])!=0){
     MA[index1,] <- MA[index1,]/sum(MA[index1,])
   }
 }
 
-## Generate S
+## Generate vector S
 e <- match(a, b)
 S <- tabulate(e)
+S <- S/sum(S) ## translate the frequency to probabilities
 
+
+## step 8
 ## Word Generation
-
-## simulation
-result <- c()
-firstWord <- sample(1:length(b), size=1, prob=S)
+firstWord <- sample(1:length(b), size=1, prob=S) ## generate the first word where the word probabilities are simply taken from S
+## generate the second word where the word probabilities are taken from A[word1,] as the first word were given
+## if fail to do so, falling back to S
 if (sum(MA[firstWord,])!=0){
   secondWord <- sample(1:length(b), size=1, prob=MA[firstWord,])
 } else{
   secondWord <- sample(1:length(b), size=1, prob=S)
 }
-wl <- c(firstWord, secondWord)
+wl <- c(firstWord, secondWord) ## store the two words in vector wl
+
+## as two words were given, use the word probabilities in T[word1, word2,], generate the 3rd word, 
+## if fail to do so, try to generate the 3rd word using the word probabilities in A[word2,],
+## if all failed, falling back to S,
+## add the 3rd word to vector wl, and move on, use 2nd and 3rd word to generate the 4th one,
+## keep going, until there are 50 word in vector wl.
 for (i in 3:50){
   first <- wl[i-2]
   second <- wl[i-1]
@@ -103,5 +118,22 @@ for (i in 3:50){
   }
   wl <- c(wl, nextWord)
 }
-wl
-wordlist <- b[wl]
+wordlist1 <- b[wl] ## translate indexes from b to words.
+
+
+## step 9
+## simulate 50-word sections of text where the word probabilities are simply taken from S
+wl2 <- c()
+for (i in 1:50){
+  word <- sample(1:length(b), size=1, prob=S)
+  wl2 <- c(wl2,word)
+}
+wordlist2 <- b[wl2]
+
+## show both result
+wordlist1
+wordlist2
+
+
+## step 10
+
